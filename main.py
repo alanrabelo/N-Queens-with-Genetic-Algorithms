dad2b56d3293e3f89881051d8bd017263af0f849
alanrabelo <alanrabelo@icloud.com>
2018-01-27 10:47:36 -0300

    Commenting and Refactoring

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ SIZE = 4
 POPULATION = 2000
 NGEN = 150
 
-def diagonals(a, invert=False):
+def sum_diagonals(a, invert=False):
 
     if invert == True:
         a = np.rot90(a)
@@ -33,31 +33,34 @@ def fitness(individual):
         arrayFromIndividual[index][value-1] += 1
 
     sumLines = sum(np.sum(np.array(arrayFromIndividual), axis=0)[np.sum(np.array(arrayFromIndividual), axis=0) > 1])
-    sumDiag1 = (sum(value > 1 for value in diagonals(arrayFromIndividual)))
-    sumDiag2 = (sum(value > 1 for value in diagonals(arrayFromIndividual, invert=True)))
+    sumDiag1 = (sum(value > 1 for value in sum_diagonals(arrayFromIndividual)))
+    sumDiag2 = (sum(value > 1 for value in sum_diagonals(arrayFromIndividual, invert=True)))
 
     return int(sumLines + sumDiag1 + sumDiag2),
 
 
+# Genetic Algorithm Initialization
 creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
 creator.create("Individual", list, fitness=creator.FitnessMin)
 
 toolbox = base.Toolbox()
 
+# Connecting toolbox with individuals and attributes
 toolbox.register("attr_int", random.randint, 1, SIZE)
 toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_int, n=SIZE)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
-
+# AG Main functions
 toolbox.register("evaluate", fitness)
 toolbox.register("mate", tools.cxOnePoint)
 toolbox.register("mutate", tools.mutUniformInt, low=1, up=SIZE, indpb=0.15)
 toolbox.register("select", tools.selTournament, tournsize=3)
 
+# Initial Population setup
 population = toolbox.population(n=POPULATION)
 
+# AG Evolutionary Process
 for gen in range(NGEN):
-
     if gen % (NGEN/10) == 0:
         print(int(gen/NGEN*100))
     offspring = algorithms.varAnd(population, toolbox, cxpb=0.5, mutpb=0.25)
@@ -66,7 +69,8 @@ for gen in range(NGEN):
         ind.fitness.values = fit
     population = toolbox.select(offspring, k=len(population))
 
-top10 = tools.selWorst(population, k=10)
+# Select best 10 samples from population
+top10 = tools.selBest(population, k=10)
 
 for top in top10:
     print(str(fitness(top)) + ' : ' + str(top))
